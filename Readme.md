353f45a99b242cf294f28c812af5e7b606583bef
Marc <gitwork@nowheremail.com>
2016-04-28 10:10:26 -0400

    Various modifications to support sound and priority parameters

--- Readme.md
+++ Readme.md
@@ -15,7 +15,7 @@ Usage: **pushover.pl _[options]_**
 
     Required command-line arguments:  
     -a APP_TOKEN        Your application's token  
-    -k USER_KEY         Your application's token  
+    -k USER_KEY         Your User token  
     -m MESSAGE          The message you want to send  
 
     Optional command-line arguments:
@@ -23,8 +23,10 @@ Usage: **pushover.pl _[options]_**
     -d DEVICE           Send the message only to DEVICE  
     -u URL              A URL to send  
     -r URL_TITLE        Optional title for the URL  
-    -h                  The message is high priority - bypass quiet hours  
-    -s UNIX_TIMESTAMP   Unix timestamp of the message  
+    -h                  The message is high priority - bypass quiet hours. Equivalent to -p 1  
+    -s UNIX_TIMESTAMP   Unix timestamp of the message 
+    -b SOUND            The Sound file to play (see https://pushover.net/api#sounds)
+    -p PRIORITY         Priority number (-2 to 2 / Lowest to Highest). Overrides -h 
     -i                  Use STDIN for message input - useful if you want to have new lines in your message
 
 ---
@@ -39,7 +41,34 @@ The message can be given either as a command line parameter following the -m arg
 2. Send a multiline message (useful for sending push notifications from Nagios)
 
         printf "%b" "This message\nwill span several lines.\nAnd \n characters will be converted to new lines!\n" | pushover.pl -a *APP-TOKEN* -k *USER-KEY* -i
-        
+       
+
+## Notes
+At of the last update of this script, the valid Sound files (-s parameter) defined on pushover.net were:
+
+  pushover - Pushover (default)
+  bike - Bike
+  bugle - Bugle
+  cashregister - Cash Register
+  classical - Classical
+  cosmic - Cosmic
+  falling - Falling
+  gamelan - Gamelan
+  incoming - Incoming
+  intermission - Intermission
+  magic - Magic
+  mechanical - Mechanical
+  pianobar - Piano Bar
+  siren - Siren
+  spacealarm - Space Alarm
+  tugboat - Tug Boat
+  alien - Alien Alarm (long)
+  climb - Climb (long)
+  persistent - Persistent (long)
+  echo - Pushover Echo (long)
+  updown - Up Down (long)
+  none - None (silent) 
+
 ## License
 pushover-client-perl is released under the MIT license
 
--- pushover.pl
+++ pushover.pl
@@ -5,13 +5,15 @@ use warnings;
 use LWP::UserAgent;
 use Getopt::Std;
 
+my $priority_setting = 0;
+
 my $options = {};
-getopt('akmtdurh:si:', $options);
+getopt('akmtdurh:sbpi:', $options);
 my $usage =<<END;
 Usage: $0 [options]
 Required:
 -a APP_TOKEN\t\tYour application's token
--k USER_KEY\t\tYour application's token
+-k USER_KEY\t\tYour user token
 -m MESSAGE\t\tThe message you want to send
 
 Optional:
@@ -19,8 +21,10 @@ Optional:
 -d DEVICE\t\tSend the message only to DEVICE
 -u URL\t\t\tA URL to send
 -r URL_TITLE\t\tOptional title for the URL
--h\t\t\tThe message is high priority - bypass quiet hours
+-h\t\t\tThe message is high priority - bypass quiet hours (equivalent to -p 1)
 -s UNIX_TIMESTAMP\tUnix timestamp of the message
+-b SOUND\t\tThe sound file to play  (see https://pushover.net/api#sounds)
+-p PRIORITY\t\tThe priority level to send. -2 (lowest) to 2 (highest). Overrides -h
 -i Use STDIN for message input - useful if you want to have new lines in your message
 END
 
@@ -32,6 +36,22 @@ if (!defined($options->{'a'}) || !defined($options->{'k'})) {
 if (!defined($options->{'m'}) && !exists($options->{'i'})) {
   print $usage;
   exit(1);
+}
+
+$priority_setting = 1 if (defined($options->{'h'}));
+
+# -p overrides priority setting
+if (defined($options->{'p'}))
+{
+  # accept -2,-1,0,1,+1,2,+2
+  if ($options->{'p'} !~ /^(-[1-2]|0|\+{0,1}[1-2])$/)
+  {
+    print "ERROR: invalid priority value\n\n";
+    print $usage;
+    exit(1);
+  }
+  # strip leading +
+  ($priority_setting) = ($options->{'p'} =~ s/^\+//g); 
 }
 
 delete $options->{'r'} if (defined($options->{'r'}) && !defined($options->{'u'}));
@@ -56,7 +76,9 @@ $push_options{'title'} = $options->{'t'} if (defined($options->{'t'}));
 $push_options{'device'} = $options->{'d'} if (defined($options->{'d'}));
 $push_options{'url'} = $options->{'u'} if (defined($options->{'u'}));
 $push_options{'url_title'} = $options->{'r'} if (defined($options->{'r'}));
-$push_options{'priority'} = 1 if (defined($options->{'h'}));
+$push_options{'priority'} = $priority_setting;
 $push_options{'timestamp'} = $options->{'s'} if (defined($options->{'s'}));
+$push_options{'sound'} = $options->{'b'} if (defined($options->{'b'}));
 
 LWP::UserAgent->new()->post("https://api.pushover.net/1/messages", \%push_options);
+
